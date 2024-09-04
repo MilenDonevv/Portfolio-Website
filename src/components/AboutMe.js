765d5b3dea9c635253958c302578f5ff1396c2ef
MilenDonevv <milendonev95@gmail.com>
2024-09-04 17:04:44 +0300

    Improve personal description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,10 +9,10 @@ export const AboutMe = () => {
         <div className="about-me-section">
           <h2 className="about-me-title">About me</h2>
           <p className="about-me-text">
-            From an early age, I’ve been fascinated by how computers and technology work, 
-            but it wasn’t until recently that I had the opportunity to delve deeper into this passion. 
-            With the ever-growing importance of technology in our increasingly digital future, 
-            I’ve come to believe that working in the technology domain is more crucial than ever. 
+            From an early age, I’ve been fascinated by how technology works, 
+            but it wasn’t until recently that I had the opportunity to delve deeper into this passion of mine. 
+            With the ever-growing importance of our technological advancements in this increasingly digital future, 
+            I’ve come to believe that working in the tech domain is more crucial than ever. 
             Outside of coding, I enjoy working out, socializing with people,
             reading books, and keeping up with the latest and greatest news from different specters of life. 
           </p>
